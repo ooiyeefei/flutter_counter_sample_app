dd7ed3ddc22d706f38fb25b97c68d909c3d7c8d9
Yee Fei <yeefeiooi@gmail.com>
2021-09-29 00:09:36 +0800

    Built web
    Branch: main
    Commit: 7b5f2f9ac7f448f0812501a386f587f702392763
    
    package:peanut 4.1.1

---
+++ pages/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'This is home page',
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
